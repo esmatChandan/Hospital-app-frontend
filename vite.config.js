37b29b471dd018bcb5c331c3c464677f36d24a7d
Chandan Kumar Shaw <147383845+esmatChandan@users.noreply.github.com>
2025-02-01 22:01:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows external access
+    port: 3000, // Set a specific port (Render may override this)
+  }
 })
